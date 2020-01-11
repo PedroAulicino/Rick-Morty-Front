2c50f360cf222e5319cdb924d30b13664b2f6827
Pedro Aulicino <pedrooaulicino@gmail.com>
2020-01-11 18:06:27 -0300

    create card and first request

--- src/page/home/home.page.jsx
+++ src/page/home/home.page.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CardComponent from '../../components/card.component'
+import API from '../../services/api'
 
 
-export default ()=>{
-    return <h2>MAIN</h2>
+
+
+
+export default () => {
+    const [chacters, setChacters] = useState([])
+    const [first, setFirst] = useState(0)
+
+    const fetchUser = async () => {
+        const user = await API().get("character")
+        console.log('aui')
+        setChacters(user.data.results)
+    }
+    useEffect(() => {
+        fetchUser()
+    }, [first])
+
+    return <div>
+        {chacters.map(data => <CardComponent key={data.id} image={data.image} name={data.name} ></CardComponent>)}
+    </div>
 }
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+
+const request = () => {
+    return axios.create({ baseURL: 'https://rickandmortyapi.com/api/' })
+}
+
+export default request
